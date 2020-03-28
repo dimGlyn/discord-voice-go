a9acfcfa0ed54f1b53fc5cafa743a052a85e70c0
Dimtiris <dim.glynatsis@gmail.com>
2020-03-28 18:08:05 +0200

    answer with madclip

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/dimGlyn/discord-voice-go
+
+go 1.14
+
+require (
+	github.com/bwmarrin/dca v0.0.0-20180611192139-99fdf68b87a1 // indirect
+	github.com/bwmarrin/discordgo v0.20.2
+	layeh.com/gopus v0.0.0-20161224163843-0ebf989153aa // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"bufio"
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-type DiscordPayload struct {
-	session *discordgo.Session
-	message *discordgo.MessageCreate
-}
-
 var (
-	Token   string
-	scanner *bufio.Scanner
+	Token  string
+	buffer = make([][]byte, 0)
 )
 
 func init() {
@@ -27,7 +24,9 @@ func init() {
 }
 
 func main() {
-	discord, err := discordgo.New("Bot " + Token)
+	discord, _ := discordgo.New("Bot " + Token)
+
+	err := loadSound("data/EEEEEEEEEEEEEEEEEEEEEE.dca")
 
 	if err != nil {
 		fmt.Println("error lol, ", err)
@@ -54,14 +53,94 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	if m.Content == "quit" || m.Content == "exit" {
-		fmt.Println("Bye bye")
+	if m.Content == "!ok" || m.Content == "!entaksei" {
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			return
+		}
+		g, err := s.State.Guild(c.GuildID)
+		if err != nil {
+			return
+		}
+		for _, vs := range g.VoiceStates {
+			if vs.UserID == m.Author.ID {
+				err = playSound(s, g.ID, vs.ChannelID)
+				if err != nil {
+					fmt.Println("Error playing sound:", err)
+				}
+
+				return
+			}
+		}
+	}
+}
+
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+
+	if event.Guild.Unavailable {
 		return
 	}
 
+	for _, channel := range event.Guild.Channels {
+		if channel.ID == event.Guild.ID {
+			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+			return
+		}
+	}
 }
 
-func (dp *DiscordPayload) respond(res string) {
-	fmt.Println(res)
-	dp.session.ChannelMessageSend(dp.message.ChannelID, res)
+func loadSound(path string) error {
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening dca file :", err)
+		return err
+	}
+
+	var opuslen int16
+
+	for {
+		err = binary.Read(file, binary.LittleEndian, &opuslen)
+
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			err := file.Close()
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			return err
+		}
+
+		InBuf := make([]byte, opuslen)
+		err = binary.Read(file, binary.LittleEndian, &InBuf)
+
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			return err
+		}
+
+		buffer = append(buffer, InBuf)
+	}
+}
+
+func playSound(s *discordgo.Session, guildID, channelID string) (err error) {
+	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
+	if err != nil {
+		return err
+	}
+	time.Sleep(250 * time.Millisecond)
+
+	vc.Speaking(true)
+	for _, buff := range buffer {
+		vc.OpusSend <- buff
+	}
+	vc.Speaking(false)
+
+	time.Sleep(250 * time.Millisecond)
+	vc.Disconnect()
+
+	return nil
 }
